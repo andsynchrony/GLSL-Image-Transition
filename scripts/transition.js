67b712844da1c71c721b2d1c105d5882c3390782
Stefan Wagner <sw@designandsystems.de>
2014-04-16 12:30:53 +0200

    implemented array loading from div class

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,20 +1,15 @@
-
-
 define(['glsl-transition'],
 	function( GlslTransition ) {
 
 	var canvas = document.getElementById("viewport");
-	var Transition = GlslTransition(canvas);
 
 	var shader = document.getElementById('squares').textContent;
-	var awesomeTransition1 = Transition(shader, { size: [13, 9], smoothness: 0.5 })
 
-	var image1 = document.getElementById('img1');
-	var image2 = document.getElementById('img2');
+	var images = document.getElementsByClassName("slideShowImg");
 
+	var transition = GlslTransition(canvas)(shader, { size: [13, 9], smoothness: 0.5 })
 
-	var squareTransition = function (x) { return x*x; };
+	var squareTransition = function (x) { return x; };
 
-	awesomeTransition1({ from: image1, to: image2 }, 2000, squareTransition);
-
+	transition({ from: images[0], to: images[1] }, 2000, squareTransition);
 });
