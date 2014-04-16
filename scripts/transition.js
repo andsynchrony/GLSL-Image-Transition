e614ed62567efaaf85ea4e141e0c9638486f641b
Stefan Wagner <sw@designandsystems.de>
2014-04-16 14:01:04 +0200

    added saturation effect to transition

--- scripts/transition.js
+++ scripts/transition.js
@@ -2,9 +2,11 @@ define(['glsl-transition'],
 	function( GlslTransition ) {
 
 	var canvas = document.getElementById("slideShow"),
-	shader = document.getElementById('squares').textContent,
+	shader = document.getElementById('transitionShader').textContent,
 	images = document.getElementsByClassName("slideShowImg"),
-	transition = GlslTransition(canvas)(shader, { size: [13, 9], smoothness: 0.5 });
+	transition = GlslTransition(canvas)(shader, { size: [8, 4.5], smoothness: 0.5 }),
+	transitionDuration = 1400,
+	transitionDelay = 1000;
 
 	var linearEasing = function (x) { return x; };
 
@@ -17,7 +19,7 @@ define(['glsl-transition'],
 	{
 		var i = i === images.length ? 0 : i;
 		var next = i + 1 === images.length ? 0 : i + 1;
-		transition({ from: images[i], to: images[next] }, 2000, linearEasing).then(function() {
+		transition({ from: images[i], to: images[next] }, transitionDuration, linearEasing).delay(transitionDelay).then(function() {
 			return loopTransition( i+1 );
 		});
 	}
