cbbfb626f4f7b2465e505e8a5385906bbcea72f7
Georg Fischer <snorpey@gmail.com>
2014-04-22 10:47:05 +0200

    cleanup

--- scripts/tools/transition.js
+++ scripts/tools/transition.js
@@ -1,99 +1,110 @@
-define([ 'tools/scaleImage', 'lib/glsl-transition', "text!tools/transitionShader.glsl"],
-	function( scaleImage, GlslTransition, transitionShader ) {
-
-	var canvas, transition, images, transitionDuration, transitionDelay, transitionCallback;
-	var canvasWidth, canvasHeight;
-	var tileWidth, tileHeight, numTilesX, numTilesY;
-	var linearEasing = function (x) { return x; };
-
-
-	function start( options )
+/*global define*/
+define(
+	['tools/scaleImage', 'lib/glsl-transition', 'text!tools/transitionShader.glsl'],
+	function ( scaleImage, glslTransition, transitionShader )
 	{
-		canvas = options.canvas;
-		transitionDuration = options.duration;
-		transitionDelay = options.delay;
-		transitionCallback = options.callback;
+		var canvas, transition, images, transitionDuration, transitionDelay, transitionCallback;
+		var canvasWidth, canvasHeight;
+		var tileWidth, tileHeight, numTilesX, numTilesY;
+		
+		function linearEasing ( x ) { return x; }
 
-		canvasWidth = options.width;
-		canvasHeight = options.height;
-
-		tileWidth = options.tileWidth;
-		tileHeight = options.tileHeight;
-		numTilesX = (canvasWidth/tileWidth);
-		numTilesY = (canvasHeight/tileHeight);
-
-		tileHeight = options.tileHeight;
-
-		transition = GlslTransition(canvas)(transitionShader, { size: [numTilesX, numTilesY], smoothness: 1.0 });
-
-		// wait for the images to be scaled, then start the loop.
-		scaleImage.scaleImages(options.images, options.width, options.height, startLoop);
-	}
-
-	function startLoop( scaledImages )
-	{
-		images = scaledImages;
-
-		// start the transition loop.
-		loopTransition(0);
-	}
-
-	function loopTransition( i )
-	{
-		transitionCallback( (i+1)%images.length );
-		var i = i === images.length ? 0 : i;
-		var next = i + 1 === images.length ? 0 : i + 1;
-		transition({ from: images[i], to: images[next] }, transitionDuration, linearEasing).delay(transitionDelay).then(function()
+		function start ( options )
 		{
-			transition = GlslTransition(canvas)(transitionShader, { size: [numTilesX, numTilesY], smoothness: 1.0 });
-			return loopTransition( i + 1 );
-		});
-	}
+			canvas = options.canvas;
+			transitionDuration = options.duration;
+			transitionDelay = options.delay;
+			transitionCallback = options.callback;
 
-	function fitToContainer(canvas, width, height, canvasWidth, canvasHeight)
-	{
-		var curW = canvasWidth;
-		var curH = canvasHeight;
-		var divWidth = width;
-		var divHeight = height;
-		var canvasRatio = canvasWidth / canvasHeight;
-		var divRatio = divWidth / divHeight;
-		if (divRatio >= canvasRatio)
-		{
-			curH = divWidth / canvasRatio;
-			curW = divWidth;
+			canvasWidth = options.width;
+			canvasHeight = options.height;
+
+			tileWidth = options.tileWidth;
+			tileHeight = options.tileHeight;
+			numTilesX = (canvasWidth/tileWidth);
+			numTilesY = (canvasHeight/tileHeight);
+
+			tileHeight = options.tileHeight;
+
+			transition = glslTransition(canvas)(transitionShader, { size: [numTilesX, numTilesY], smoothness: 1.0 });
+
+			// wait for the images to be scaled, then start the loop.
+			scaleImage.scaleImages(options.images, options.width, options.height, startLoop);
 		}
-		else
+
+		function startLoop ( scaledImages )
 		{
-			curW = divHeight * canvasRatio;
-			curH = divHeight;
+			images = scaledImages;
+
+			// start the transition loop.
+			loopTransition(0);
 		}
-		// tolerant browser window needs some tolerance.
-		if( curW + 50 > divWidth )
+
+		function loopTransition ( i )
 		{
-			canvas.style.left = '-'+((curW - divWidth)/2)+'px';
+			var j = i === images.length ? 0 : i;
+			transitionCallback( (i + 1) % images.length );
+			var next = j + 1 === images.length ? 0 : j + 1;
+			
+			transition( { from: images[j], to: images[next] }, transitionDuration, linearEasing)
+				.delay(transitionDelay)
+				.then(
+					function()
+						{
+						transition = glslTransition(canvas)(transitionShader, { size: [numTilesX, numTilesY], smoothness: 1.0 });
+						return loopTransition( j + 1 );
+					}
+				);
 		}
-		if( curH + 50 > divHeight )
+
+		function fitToContainer ( canvas, width, height, canvasWidth, canvasHeight )
 		{
-			canvas.style.top = '-'+((curH - divHeight)/2)+'px';
+			var curW = canvasWidth;
+			var curH = canvasHeight;
+			var divWidth = width;
+			var divHeight = height;
+			var canvasRatio = canvasWidth / canvasHeight;
+			var divRatio = divWidth / divHeight;
+			
+			if (divRatio >= canvasRatio)
+			{
+				curH = divWidth / canvasRatio;
+				curW = divWidth;
+			}
+			else
+			{
+				curW = divHeight * canvasRatio;
+				curH = divHeight;
+			}
+			
+			// tolerant browser window needs some tolerance.
+			if ( curW + 50 > divWidth )
+			{
+				canvas.style.left = '-'+((curW - divWidth)/2)+'px';
+			}
+			
+			if ( curH + 50 > divHeight )
+			{
+				canvas.style.top = '-'+((curH - divHeight)/2)+'px';
+			}
+
+			canvas.style.width  = curW + 'px';
+			canvas.style.height = curH + 'px';
+			canvas.width  = canvas.offsetWidth;
+			canvas.height = canvas.offsetHeight;
 		}
-		canvas.style.width =curW+'px';
-		canvas.style.height=curH+'px';
-		canvas.width  = canvas.offsetWidth;
-		canvas.height = canvas.offsetHeight;
+
+		function resize ( width, height )
+		{
+			transition.reset();
+			numTilesX = (width / tileWidth);
+			numTilesY = (height / tileHeight);
+			fitToContainer(canvas, width, height, canvasWidth, canvasHeight);
+		}
+
+		return {
+			start : start,
+			resize : resize
+		};
 	}
-
-	function resize(width, height)
-	{
-
-		transition.reset();
-		numTilesX = (width/tileWidth);
-		numTilesY = (height/tileHeight);
-        fitToContainer(canvas, width, height, canvasWidth, canvasHeight);
-	}
-
-	return {
-		start : start,
-		resize : resize
-	};
-});
+);
